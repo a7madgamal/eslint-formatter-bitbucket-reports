8cfe100601280c079b743d15faa489ee8fb1afdb
Ahmed Hassanein <ahmed.hassanein@curalie.com>
2023-07-18 15:02:06 +0200

    fix summary limit issue

--- index.ts
+++ index.ts
@@ -88,7 +88,7 @@ function generateAnnotations(
           external_id,
           line,
           path: relativePath,
-          summary: `${message} (${ruleId})`,
+          summary: `${message.substring(400)} (${ruleId})`,
           annotation_type: "BUG",
           severity: severity === 1 ? SEVERITIES.MEDIUM : SEVERITIES.HIGH,
         };
